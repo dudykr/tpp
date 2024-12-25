463fa753aebd68858e5453463f0020d2f118cea3
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-12-26 07:28:24 +0900

    gitbub auth

--- apps/webapp/src/lib/server/auth-options.ts
+++ apps/webapp/src/lib/server/auth-options.ts
@@ -1,12 +1,16 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/server/db";
-import EmailProvider from "next-auth/providers/email";
-import GithubProvider from "next-auth/providers/github";
 import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
   adapter: DrizzleAdapter(db),
-  providers: [EmailProvider({})],
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
   callbacks: {
     session: ({ session, user }) => ({
       ...session,
