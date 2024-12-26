bd64ba953c57dbd82cb54c579480088441c673bc
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-12-26 15:04:42 +0900

    fix

--- apps/webapp/src/server/google.ts
+++ apps/webapp/src/server/google.ts
@@ -1,3 +1,10 @@
-export const googleCredentials = JSON.parse(
-  Buffer.from(process.env.GOOGLE_SERVICE_ACCOUNT_JSON!, "base64").toString(),
-);
+import "server-only";
+
+export const googleCredentials = process.env.GOOGLE_SERVICE_ACCOUNT_JSON
+  ? JSON.parse(
+      Buffer.from(
+        process.env.GOOGLE_SERVICE_ACCOUNT_JSON!,
+        "base64",
+      ).toString(),
+    )
+  : undefined;
